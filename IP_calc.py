a6c85e79a47b55e06f59721759da9cf2518771f9
OlMotova <olmotova@mail.ru>
2024-08-02 23:09:39 +0300

    some adds to check

--- IP_calc.py
+++ IP_calc.py
@@ -3,22 +3,25 @@ i = 0
 while i < 20:
     try:
         StN = int(input("Введите стартовый номер: "))
-    except ValueError: print("введите ЧИСЛО")
+    except ValueError: print("ОШИБКА: введите ЧИСЛО")
 
     else:
         if StN in range(0, 256):
             try:
                 ZvN = int(input("Введите заводской номер: "))
-            except ValueError: print("введите ЧИСЛО")
+            except ValueError: print("ОШИБКА: введите ЧИСЛО")
 
             else:
-                IP = ipaddress.IPv4Address(u'10.8.0.0') + (ZvN // 256 + StN) * 256 + ZvN % 256
-                if IP >= ipaddress.IPv4Address(u'10.9.0.0') or IP < ipaddress.IPv4Address(u'10.8.0.0'):
-                    print ("IP вне диапазона 10.8")
+                if ZvN < 0: print("ОШИБКА: номер должен быть больше 0")
+
                 else:
-                    print(f"IP устройства: {IP}")
-                    i += 1
-        else: print("Стартовый номер должен быть в пределах от 0 до 255")
+                    IP = ipaddress.IPv4Address(u'10.8.0.0') + (ZvN // 256 + StN) * 256 + ZvN % 256
+                    if IP >= ipaddress.IPv4Address(u'10.9.0.0') or IP < ipaddress.IPv4Address(u'10.8.0.0'):
+                        print ("ОШИБКА: IP вне диапазона 10.8")
+                    else:
+                        print(f"IP устройства: {IP}")
+                        i += 1
+        else: print("ОШИБКА: Стартовый номер должен быть в пределах от 0 до 255")
 else: print("Пора отдохнуть")
 
 
