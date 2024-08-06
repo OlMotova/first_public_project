b7adadceef89d2b07d5a627ce4efb1484657fa8e
OlMotova <olmotova@mail.ru>
2024-08-06 12:04:01 +0300

    additional projecr with CAMeras: accessing cells

--- CAM/CAM_code.py
+++ CAM/CAM_code.py
@@ -8,13 +8,16 @@ van_num = int (input("Введите номер вагона: "))
 cam_cam = cam[cam["van_num"] == van_num]
 print(cam_cam)
 
-print(f'''"IP Cam1" - {cam_cam["Cam1"]},
-"IP Cam2" - {cam_cam["Cam2"]},
-"IP Cam3" - {cam_cam["Cam3"]},
-"IP Cam4" - {cam_cam["Cam4"]},
-"IP bes1" - {cam_cam["bes1"]},
-"IP bes2" - {cam_cam["bes2"]},
-"IP bes3" - {cam_cam["bes3"]},
-"IP bes4" - {cam_cam["bes4"]},
+cam_cam.index = range(0,1)
+
+print(f'''"IP Cam1" - {cam_cam["Cam1"][0]},
+"IP Cam2" - {cam_cam["Cam2"][0]},
+"IP Cam3" - {cam_cam["Cam3"][0]},
+"IP Cam4" - {cam_cam["Cam4"][0]},
+"IP bes1" - {cam_cam["bes1"][0]},
+"IP bes2" - {cam_cam["bes2"][0]},
+"IP bes3" - {cam_cam["bes3"][0]},
+"IP bes4" - {cam_cam["bes4"][0]}.
 ''')
+
 
