7a0745c40cba13abd869a76abbcf842b9989b632
OlMotova <olmotova@mail.ru>
2024-08-09 18:07:22 +0300

    START NEW Project IP_calculation_Pro

---
+++ IP_calculation_Pro/IP_calculation.py
@@ -0,0 +1,37 @@
+import pandas as pd
+
+url = 'https://docs.google.com/spreadsheets/d/1mPWR7CGOOu268woDhUocSk2owEBvMrNEzFNC2t6ArCU/edit?usp=sharing'
+
+import re
+
+def convert_google_sheet_url(url):
+    # Regular expression to match and capture the necessary part of the URL
+    pattern = r'https://docs\.google\.com/spreadsheets/d/([a-zA-Z0-9-_]+)(/edit#gid=(\d+)|/edit.*)?'
+
+    # Replace function to construct the new URL for CSV export
+    # If gid is present in the URL, it includes it in the export URL, otherwise, it's omitted
+    replacement = lambda m: f'https://docs.google.com/spreadsheets/d/{m.group(1)}/export?' + (f'gid={m.group(3)}&' if m.group(3) else '') + 'format=csv'
+
+    # Replace using regex
+    new_url = re.sub(pattern, replacement, url)
+
+    return new_url
+
+new_url = convert_google_sheet_url(url)
+
+print(new_url)
+# https://docs.google.com/spreadsheets/d/1mSEJtzy5L0nuIMRlY9rYdC5s899Ptu2gdMJcIalr5pg/export?gid=1606352415&format=csv
+
+
+df = pd.read_csv(new_url)
+
+print(df)
+
+print('________________________')
+
+#df_2 = df[df['switch_last_serial_number'] == ''] yt hf,jnftn
+
+df_2 = pd.isna(df['switch_last_serial_number'])
+df_3 = df['switch_last_serial_number']
+
+print(df_2)
