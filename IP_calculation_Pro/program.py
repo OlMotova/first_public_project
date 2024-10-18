fcb9a5e1cc1cb3ba3acdaa779a1f0a4947fd5559
OlMotova <olmotova@mail.ru>
2024-10-18 17:49:04 +0300

    program.py updated

--- IP_calculation_Pro/program.py
+++ IP_calculation_Pro/program.py
@@ -23,24 +23,29 @@ df_3 = df_filter(df_2, 'equipment_type', equipment_type)
 equipment_df_3 = df_filter(equipment_df_2, 'equipment_type', equipment_type)
 
 stickers_count = int(input("Введите колличество этикеток: "))
+#______________________________________
 
-equipment_index = equipment_df_3.index #РАЗОБРАТЬСЯ!!!!!!!!!!!!!!
-equipment_df_3 = index_reset(equipment_df_3)
+def save_button (df, df_3, equipment_df_3, stickers_count):
+    equipment_index = equipment_df_3.index #РАЗОБРАТЬСЯ!!!!!!!!!!!!!!
+    equipment_df_3 = index_reset(equipment_df_3)
 
-last_order_number = last_order_number_func (df)
+    last_order_number = last_order_number_func (df)
 
-df_add = {}
+    df_add = {}
 
-df_add = new_order_creating(df_3, equipment_df_3, stickers_count, last_order_number, equipment_name, equipment_type)
-#LAUNCHING THE MAIN FUNCTION!!!
+    df_add = new_order_creating(df_3, equipment_df_3, stickers_count, last_order_number, equipment_name, equipment_type)
+    #LAUNCHING THE MAIN FUNCTION!!!
 
 
 
-equipment_index_int = int(equipment_index[0])
-IP_range_check(equipment_df, equipment_index_int)
+    equipment_index_int = int(equipment_index[0])
+    IP_range_check(equipment_df, equipment_index_int)
 
-df = df._append(df_add, ignore_index= True)
+    df = df._append(df_add, ignore_index= True)
 
-df.to_excel("Main_file.xlsx", index=False) #Main file updating with new line/order
+    df.to_excel("Main_file.xlsx", index=False) #Main file updating with new line/order
+
+save_button (df, df_3, equipment_df_3, stickers_count)
+
 
 print("Дальше код не написан")
