c1acb6a20adb3c5112630c4e3429f96165ff5b37
OlMotova <olmotova@mail.ru>
2024-10-22 14:05:32 +0300

    functions.py updated with saving and 3 lines output

--- IP_calculation_Pro/functions.py
+++ IP_calculation_Pro/functions.py
@@ -158,23 +158,29 @@ def IP_range_check_and_text_output (df_base, index, new_first_ZvN, new_last_ZvN,
 
         if flag == 'save_flag':
             text = f'''
-________________________________________
-Информация о новом заказе № {order_number} внесена в базу.
+Заказ № {order_number}.
 
-Требуется заказать этикетки для {equipment_name} {equipment_type}:
+Оборудование: {equipment_name} {equipment_type}
+
+Требуемый диапазон:
 
 {new_first_ZvN} - {new_first_IP}, {new_first_ZvN + 1} - {new_first_IP + 1}, ..., {new_last_ZvN} - {new_last_IP}
-    
+
+________________________________________
+Информация о новом заказе № {order_number} внесена в базу.    
 '''
             with open('output.txt', 'w') as f, redirect_stdout(f):
                  print(f'''
-________________________________________
-Информация о новом заказе № {order_number} внесена в базу.
+Заказ № {order_number}.
 
-Требуется заказать этикетки для {equipment_name} {equipment_type}:
+Оборудование: {equipment_name} {equipment_type}
+
+Требуемый диапазон:
 
 {new_first_ZvN} - {new_first_IP}, {new_first_ZvN + 1} - {new_first_IP + 1}, ..., {new_last_ZvN} - {new_last_IP}
-    
+
+________________________________________
+Информация о новом заказе № {order_number} внесена в базу.  
 ''')
 
         else:
@@ -201,7 +207,8 @@ ________________________________________
     #
     # ''')
 
-    else:
+    elif int(last_IP_parts[2]) - int(first_IP_parts[2]) == 1:
+        print(f"ОТДЕЛЬНЫЕ части  {int(first_IP_parts[2])}, {int(last_IP_parts[2])}, {int(last_IP_parts[2]) - int(first_IP_parts[2])}")
         print("\nнадо поработать над разделением диапазонов")
         middle_IP = middle_IP_creating(first_IP_parts[0], first_IP_parts[1], first_IP_parts[2])
         part_of_stickers_count = 255 - int(first_IP_parts[3])
@@ -238,18 +245,142 @@ ________________________________________
             line_1 = f'{new_first_ZvN} - {new_first_IP}, {new_first_ZvN + 1} - {new_first_IP + 1}, ... , {middle_ZvN} - {middle_IP}'
             line_2 = f'{middle_ZvN + 1} - {middle_IP + 1}, {middle_ZvN + 2} - {middle_IP + 2}, ... , {new_last_ZvN} - {new_last_IP}'
 
-        text = f'''
+        if flag == 'save_flag':
+            text = f'''
+Заказ № {order_number}.
+
+Оборудование: {equipment_name} {equipment_type}
+
+Требуемый диапазон:
+
+{line_1}, 
+{line_2}.    
+
+________________________________________
+Информация о новом заказе № {order_number} внесена в базу.    
+'''
+            with open('output.txt', 'w') as f, redirect_stdout(f):
+                print(f'''
+Заказ № {order_number}.
+
+Оборудование: {equipment_name} {equipment_type}
+
+Требуемый диапазон:
+
+{line_1}, 
+{line_2}. 
+
+________________________________________
+Информация о новом заказе № {order_number} внесена в базу.  
+''')
+        else:
+
+            text = f'''
+    
+    Результат расчета этикеток для {equipment_name} {equipment_type}:
+    
+    {line_1}, 
+    {line_2}.
+    
+        '''
+
+    elif int(last_IP_parts[2]) - int(first_IP_parts[2]) == 2:
+        print(
+            f"ОТДЕЛЬНЫЕ части  {int(first_IP_parts[2])}, {int(last_IP_parts[2])}, {int(last_IP_parts[2]) - int(first_IP_parts[2])}")
+        print("\nнадо поработать над разделением диапазонов")
+        middle_IP_1 = middle_IP_creating(first_IP_parts[0], first_IP_parts[1], first_IP_parts[2])
+        part_of_stickers_count = 255 - int(first_IP_parts[3])
+        middle_ZvN_1 = new_first_ZvN + part_of_stickers_count
+
+        middle_IP_2 = middle_IP_creating(first_IP_parts[0], first_IP_parts[1], int(first_IP_parts[2]) + 1)
+
+        middle_ZvN_2 = new_first_ZvN + part_of_stickers_count + 256
+
+
+        list_IP_1 = create_IP_list(new_first_IP, middle_IP_1)
+        list_IP_2 = create_IP_list(middle_IP_1 + 1, middle_IP_2)
+        list_IP_3 = create_IP_list(middle_IP_2 + 1, new_last_IP)
+
+        list_ZvN_1 = create_IP_list(new_first_ZvN, middle_ZvN_1)
+        list_ZvN_2 = create_IP_list(middle_ZvN_1 + 1, middle_ZvN_2)
+        list_ZvN_3 = create_IP_list(middle_ZvN_2 + 1, new_last_ZvN)
+
+        list_1 = [' - '.join(x) for x in zip(map(str, list_ZvN_1), map(str, list_IP_1))]
+        list_2 = [' - '.join(x) for x in zip(map(str, list_ZvN_2), map(str, list_IP_2))]
+        list_3 = [' - '.join(x) for x in zip(map(str, list_ZvN_3), map(str, list_IP_3))]
+        print(f'''
+        {middle_IP_1} - {middle_IP_2}
+        {list_1}
+        {list_2}
+        {list_3}
+        ''')
+        if len(list_IP_1) < 4 and len(list_IP_3) < 4:
+            line_1 = ', '.join(map(str, list_1))
+            line_2 = f'{middle_ZvN_1 + 1} - {middle_IP_1 + 1}, {middle_ZvN_1 + 2} - {middle_IP_1 + 2}, ... , {middle_ZvN_2} - {middle_IP_2}'
+            line_3 = ', '.join(map(str, list_3))
+        elif len(list_IP_1) < 4:
+            line_1 = ', '.join(map(str, list_1))
+            line_2 = f'{middle_ZvN_1 + 1} - {middle_IP_1 + 1}, {middle_ZvN_1 + 2} - {middle_IP_1 + 2}, ... , {middle_ZvN_2} - {middle_IP_2}'
+            line_3 = f'{middle_ZvN_2 + 1} - {middle_IP_2 + 1}, {middle_ZvN_2 + 2} - {middle_IP_2 + 2}, ... , {new_last_ZvN} - {new_last_IP}'
+        elif len(list_IP_2) < 4:
+            line_1 = f'{new_first_ZvN} - {new_first_IP}, {new_first_ZvN + 1} - {new_first_IP + 1}, ... , {middle_ZvN_1} - {middle_IP_1}'
+            line_2 = f'{middle_ZvN_1 + 1} - {middle_IP_1 + 1}, {middle_ZvN_1 + 2} - {middle_IP_1 + 2}, ... , {middle_ZvN_2} - {middle_IP_2}'
+            line_3 = ', '.join(map(str, list_3))
+        else:
+            line_1 = f'{new_first_ZvN} - {new_first_IP}, {new_first_ZvN + 1} - {new_first_IP + 1}, ... , {middle_ZvN_1} - {middle_IP_1}'
+            line_2 = f'{middle_ZvN_1 + 1} - {middle_IP_1 + 1}, {middle_ZvN_1 + 2} - {middle_IP_1 + 2}, ... , {middle_ZvN_2} - {middle_IP_2}'
+            line_3 = f'{middle_ZvN_2 + 1} - {middle_IP_2 + 1}, {middle_ZvN_2 + 2} - {middle_IP_2 + 2}, ... , {new_last_ZvN} - {new_last_IP}'
+
+        if flag == 'save_flag':
+            text = f'''
+Заказ № {order_number}.
+
+Оборудование: {equipment_name} {equipment_type}
+
+Требуемый диапазон:
+
+{line_1}, 
+{line_2},
+{line_3}.  
+
+________________________________________
+Информация о новом заказе № {order_number} внесена в базу.    
+'''
+            with open('output.txt', 'w') as f, redirect_stdout(f):
+                    print(f'''
+Заказ № {order_number}.
+
+Оборудование: {equipment_name} {equipment_type}
+
+Требуемый диапазон:
+
+{line_1}, 
+{line_2},
+{line_3}.
+
+________________________________________
+Информация о новом заказе № {order_number} внесена в базу.  
+''')
+        else:
+            text = f'''
 
 Результат расчета этикеток для {equipment_name} {equipment_type}:
 
 {line_1}, 
-{line_2}.
+{line_2},
+{line_3}.
 
-    '''
+'''
 
 
-        print(text)
-        return text, True
+    else:
+
+        text = 'А не дофига ли???'
+        #text = f"ВНИМАНИЕ!!! {int(first_IP_parts[2])}, {int(last_IP_parts[2])}, {int(last_IP_parts[2]) - int(first_IP_parts[2])}"
+        return text, False
+
+    print(text)
+    return text, True
 
 
     #     with open('output.txt', 'w') as f, redirect_stdout(f):
