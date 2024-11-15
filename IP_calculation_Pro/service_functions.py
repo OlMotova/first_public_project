2dbc3c5d10268d943c52887789c8774b737dd633
OlMotova <olmotova@mail.ru>
2024-11-15 17:19:26 +0300

    добавила выводы текста при ошибках как первая из 4 переменных calculating_button_service

--- IP_calculation_Pro/service_functions.py
+++ IP_calculation_Pro/service_functions.py
@@ -17,8 +17,10 @@ def check_IP_range_overlap(df_base_IP, first_IP, last_IP):
 
         # Check if the input range overlaps with any range in the table
         if first_IP <= range_end and range_start <= last_IP:
-            return False
-    return True
+            text = f'Есть пересечение у введенного диапазона и строки {i + 2} таблица equipment_IP_database.xlsx'
+            return False, text
+    text = 'все ОК'
+    return True, text
 
 def check_IP_range_overlap_in_file(df_base_IP):
     # Convert IP ranges to actual IP objects for comparison
@@ -35,10 +37,10 @@ def check_IP_range_overlap_in_file(df_base_IP):
 
             # Check if the ranges overlap
             if range1_start <= range2_end and range2_start <= range1_end:
-                print(f"Строка файла equipment_IP_database.xlsx {i+2} пересекается со строкой {j+2}")
-                return False
-
-    return True
+                text = f"Строка файла equipment_IP_database.xlsx {i+2} пересекается со строкой {j+2}"
+                return False, text
+    text = 'все ОК'
+    return True, text
 
 def check_ip_serial_difference(df_base_IP):
     # Convert IP ranges to actual IP objects for comparison
@@ -57,10 +59,10 @@ def check_ip_serial_difference(df_base_IP):
 
         # Check if the differences are not equal
         if ip_range_diff != serial_range_diff:
-            print(f'Ошибка диапазонов в строке {i + 2}')
-            return False
-
-    return True
+            text = f'Ошибка диапазонов в строке {i + 2}'
+            return False, text
+    text = 'все ОК'
+    return True, text
 
 def new_equipment_df_line_creating(equipment_name, equipment_type,  new_first_ZvN, new_last_ZvN, new_first_IP, new_last_IP, ab_label): #Returns df line with new data for adding to eq_base file
 
@@ -76,17 +78,21 @@ def new_equipment_df_line_creating(equipment_name, equipment_type,  new_first_Zv
     return df_add
 def calculating_button_service (df_base_IP, equipment_name, equipment_type, first_ip, last_ip):
     #проверяем базу
-    if check_IP_range_overlap_in_file(df_base_IP) == False:
+    check, text = check_IP_range_overlap_in_file(df_base_IP)
+    if check == False:
         print("Ошибка на моменте check_IP_range_overlap_in_file")
-        return False, None, None
-    if check_ip_serial_difference(df_base_IP) == False:
+        return text, False, None, None
+
+    check, text = check_ip_serial_difference(df_base_IP)
+    if check == False:
         print("Ошибка на моменте check_ip_serial_difference")
-        return False, None, None
+        return text, False, None, None
 
     #проверяем пересечения диапазонов новых IP с данными в таблице
-    if check_IP_range_overlap(df_base_IP, first_ip, last_ip) == False:
+    check, text = check_IP_range_overlap(df_base_IP, first_ip, last_ip)
+    if check == False:
         print("Ошибка на моменте check_IP_range_overlap(df_base_IP, first_ip, last_ip)")
-        return False, None, None
+        return text, False, None, None
 
     equipment_df_1 = df_filter(df_base_IP, 'equipment_name', equipment_name)
     equipment_df_2 = df_filter(equipment_df_1, 'equipment_type', equipment_type)
@@ -103,9 +109,10 @@ def calculating_button_service (df_base_IP, equipment_name, equipment_type, firs
     #проверка IP на корректность порядка ввода
     if (int(last_IP) - int(first_IP)) < 1:
         print("Ошибка на моменте (int(last_IP) - int(first_IP)) < 1")
-        return False, None, None
-
-    return True, first_ZvN, last_ZvN
+        text = "первый введенный IP больше второго"
+        return text, False, None, None
+    text = "все ок"
+    return text, True, first_ZvN, last_ZvN
 
 def save_button_service (df_base_types, df_base_IP, equipment_name, equipment_type, equipment_version, first_ZvN, last_ZvN, first_IP, last_IP, new = None):
 
